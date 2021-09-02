f47c738e8ea89caae88214b0000670c894ffa7a8
zakabdi <zakariaabdi104@gmail.com>
2021-09-02 00:14:32 -0400

    finished the assignment

--- github-user-api/src/App.js
+++ github-user-api/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+
+import React from "react"
+import Profile from "./components/Profile"
+import axios from "axios"
+import "./styles.css";
+
+class App extends React.Component {
+
+  state = {
+    users: [],
+    anotherstate: []
+  }
+
+  // const [users, setUsers] = useState([])
+
+  componentDidMount() {
+
+    
+    axios.get('https://api.github.com/users/duraanali ')
+    .then(response => {
+      // console.log(response.data)
+
+    
+      this.setState({
+        ...this.state.users,
+        users:response.data,
+      })
+  
+    })
+    .catch(error => {
+      console.log( "something went wrong", error)
+    });
+
+
+   
+    
+
+
+   
+
+
+  }
+profile
+  render() {
+    console.log(this.state.users)
+
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+          <h1>List of profiles</h1>
+      <div className="App">
+          <Profile profilles={this.state.users} />
+          </div>
+      </div>
   );
+  }
 }
 
 export default App;
